75dfd98c58338e2ac9c9f381024b648777c3d616
Marc R. Preston <phyrenight@gmail.com>
2015-12-12 11:00:11 -0800

    add bingo board

--- javascript/interactivity.js
+++ javascript/interactivity.js
@@ -1,10 +1,24 @@
 randomMax = 50;
 randomMin = 1;
-var i = 0
+var i = 0;
 function assignM(){
    document.getElementById("number_place").innerHTML = "Free";
+   buildCard();
 }
 function getNumber(){
    number = Math.floor(Math.random() * (randomMax - randomMin) + randomMin);
    document.getElementById("number_place").innerHTML = number; 
+}
+
+
+function buildCard(){
+	var x = 0;
+	while (x < 5) {
+	number = Math.floor(Math.random() * (randomMax - randomMin) + randomMin);
+    var dnode = document.createElement("div");
+    var numholder = document.createTextNode(number);
+    dnode.appendChild(numholder);
+    document.getElementById("bingo_card").appendChild(dnode);
+	x++;
+	}	
 }
