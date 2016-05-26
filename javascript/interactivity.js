073e3ca61d066085e2c002e9e4d25d3fb167249c
Marc <phyrenight@gmail.com>
2016-05-26 07:03:21 -0400

    refactored code
    Sections and divs for the board are now generated by javascript. getNumber()
    has been made so other functions can use it.Most global variables have been
     deleted and replaced with local versions.

--- javascript/interactivity.js
+++ javascript/interactivity.js
@@ -1,24 +1,62 @@
-randomMax = 50;
-randomMin = 1;
-var i = 0;
-function assignM(){
-   document.getElementById("number_place").innerHTML = "Free";
-   buildCard();
-}
-function getNumber(){
-   number = Math.floor(Math.random() * (randomMax - randomMin) + randomMin);
-   document.getElementById("number_place").innerHTML = number; 
+var bingoArray = []
+function getNumber(min, max){
+   number = Math.floor(Math.random() * (max - min) + min);
+   return number 
 }
 
+function getCardData(){
+  var min = 1;
+  var max = 60;
+
+  for(var i = 0; i <= 25; i++){
+    if(i == 12) {
+      bingoArray[i] = "Free space";
+    }
+    else{
+    bingoArray[i] = getNumber(min, max)
+  }
+}
+}
+function newGame(){
+  getCardData();
+  buildCard();
+ }
 
 function buildCard(){
-	var x = 0;
-	while (x < 5) {
-	number = Math.floor(Math.random() * (randomMax - randomMin) + randomMin);
-    var dnode = document.createElement("div");
-    var numholder = document.createTextNode(number);
-    dnode.appendChild(numholder);
-    document.getElementById("bingo_card").appendChild(dnode);
-	x++;
-	}	
+    count = 1;
+    for(var i = 0; i <= 25; i++){
+       if(count == 1 || (count % 5) == 0){
+        sectionNumber = "section" + i;
+        section = document.createElement("section");
+        section.setAttribute("id", sectionNumber);
+        section.className = "row";
+        document.getElementById("bingoCard").appendChild(section);
+        
+        //section = "<section class='row' id='" + sectionNumber + "'> </section>";
+        divNumber = "div" + i;
+        //newDiv = document.getElementById(sectionNumber);
+        div = document.createElement("div");
+        div.setAttribute("id", divNumber);
+        num = document.createTextNode(i);
+        div.appendChild(num);
+        //div = "<div class='col-sm-1 col-md-1 col-lg-1 " + divNumber +"'>"
+       // + bingoArray[i] + "</div>";
+        document.getElementById("bingoCard").appendChild(div);
+       }
+       else {
+          divNumber = "div" + i;
+          div = document.createElement("div");
+          div.setAttribute("id", divNumber);
+          num = document.createTextNode(i);
+          div.appendChild(num);
+          document.getElementById(sectionNumber).appendChild(div);
+
+          //div = "<div class='col-sm-1 col-md-1 col-lg-1 " + divNumber + "'>"
+          //+ bingoArray[i] + "</div>";
+          //newDiv.innerHTML = div;
+          //count++;
+       }
+    count += 1;
+    }
 }
+
