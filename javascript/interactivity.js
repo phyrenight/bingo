c804a29539eabb75854837499c1624746988e448
Marc <phyrenight@gmail.com>
2016-05-26 07:35:00 -0400

    fixed row alignment in bingo table

--- javascript/interactivity.js
+++ javascript/interactivity.js
@@ -24,38 +24,28 @@ function newGame(){
 
 function buildCard(){
     count = 1;
-    for(var i = 0; i <= 25; i++){
-       if(count == 1 || (count % 5) == 0){
+    for(var i = 0; i < 25; i++){
+       if(count == 1 || count == 6){
         sectionNumber = "section" + i;
         section = document.createElement("section");
         section.setAttribute("id", sectionNumber);
         section.className = "row";
         document.getElementById("bingoCard").appendChild(section);
-        
-        //section = "<section class='row' id='" + sectionNumber + "'> </section>";
-        divNumber = "div" + i;
-        //newDiv = document.getElementById(sectionNumber);
-        div = document.createElement("div");
-        div.setAttribute("id", divNumber);
-        num = document.createTextNode(i);
-        div.appendChild(num);
-        //div = "<div class='col-sm-1 col-md-1 col-lg-1 " + divNumber +"'>"
-       // + bingoArray[i] + "</div>";
-        document.getElementById("bingoCard").appendChild(div);
-       }
-       else {
-          divNumber = "div" + i;
-          div = document.createElement("div");
-          div.setAttribute("id", divNumber);
-          num = document.createTextNode(i);
-          div.appendChild(num);
-          document.getElementById(sectionNumber).appendChild(div);
+        count = 1
+        }
+    divNumber = "div" + i;
+    div = document.createElement("div");
+    div.setAttribute("id", divNumber);
+    div.className = " col-sm-1 col-md-1 col-lg-1";          
+    num = document.createTextNode(i);
+    div.appendChild(num);
+    document.getElementById(sectionNumber).appendChild(div);
 
           //div = "<div class='col-sm-1 col-md-1 col-lg-1 " + divNumber + "'>"
           //+ bingoArray[i] + "</div>";
           //newDiv.innerHTML = div;
           //count++;
-       }
+       
     count += 1;
     }
 }
